e31c6ada9cbc4789867978704ae615652a222bac
leul <leulb1916@gmail.com>
2026-07-03 22:18:48 +0300

    first commit

---
+++ flask py.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = ""
+
+    if request.method == "POST":
+        try:
+            expression = request.form["expression"]
+            result = eval(expression)  # simple calculator logic
+        except:
+            result = "Error"
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
